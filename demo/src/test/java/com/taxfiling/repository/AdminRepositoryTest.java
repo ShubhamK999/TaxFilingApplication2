0895d090cf5cfd839d547de6c48299e4ebbe3e54
Shubham <02shubhamkulkarni@gmail.com>
2021-01-04 11:56:17 +0530

    done

--- demo/src/test/java/com/taxfiling/repository/AdminRepositoryTest.java
+++ demo/src/test/java/com/taxfiling/repository/AdminRepositoryTest.java
@@ -1,25 +1,12 @@
 package com.taxfiling.repository;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.taxfiling.TaxFilingApplication;
-import com.taxfiling.controller.RegistrationController;
 import com.taxfiling.entity.Admin;
 
 import junit.framework.Assert;
@@ -27,38 +14,17 @@ import junit.framework.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TaxFilingApplication.class)
-//@DataJpaTest
-//@ContextConfiguration(classes = TaxFilingApplication.class)
-//@WebMvcTest(value = AdminRepository.class)
+
  class AdminRepositoryTest {
 	
 	@Autowired
 	private AdminRepository ar;
 	
-	//@Autowired
-	// private TestEntityManager testEntityManager;
-	
-	
 	@Test
 	 void getloginAdmin() {
-		/*Admin a1 = new Admin();
-		a1.setEmail("ssk@gmail.com");
-		a1.setPassword("sk1234");
-		Admin a2 = testEntityManager.persist(a1);*/
 		Admin a3 = ar.loginAdmin("sk@cg.com", "sk123");
-		//System.out.println(a3.getEmail());
-		//assert a3.getEmail().equals("sk@cg.com") : "Test Failed";
 		Assert.assertEquals(a3.getEmail(), "sk@cg.com");
 	}
 
-	/*@Test
-	 void getloginAdminTest() {
-		Admin admin = new Admin();
-		admin.setEmail("sk@cg.com");
-		admin.setPassword("sk123");
-		Mockito.when(ar.loginAdmin(Mockito.anyString(), Mockito.anyString())).thenReturn(admin);
-		//assertEquals(ar.loginAdmin("sk@cg.com", "sk123"), admin);
-		assert admin.getPassword().equals("sk123") : "Test Failed";
-	}*/
-
+	
 }
--- demo/src/test/java/com/taxfiling/repository/CustomerRepositoryTest.java
+++ demo/src/test/java/com/taxfiling/repository/CustomerRepositoryTest.java
@@ -4,15 +4,12 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.taxfiling.TaxFilingApplication;
 import com.taxfiling.entity.Customer;
-import com.taxfiling.entity.TaxForm;
-
 import junit.framework.Assert;
 
 
